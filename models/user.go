be9d20c7f8d6079187087f7f1b204bcd8580c517
Jean C. Contreras <jean.cntrs@gmail.com>
2020-05-29 21:07:28 -0400

    Creating the user model

---
+++ models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// User : Es el modelo de usuario de la base de datos en MongoDB
+type User struct {
+	ID        primitive.ObjectID `bson: "_id, omitempty" json:"id"`
+	Names     string             `bson: "names" bson: "names, omitempty"`
+	Surnames  string             `bson: "surnames" bson: "surnames, omitempty"`
+	Birthdate time.Time          `bson: "birthdate" json:"birthdate", omitempty`
+	Email     string             `bson:"email" json:"email"`
+	Password  string             `bson:"password" json:"password, omitempty"`
+	Avatar    string             `bson: "avatar" json:"avatar, omitempty"`
+	Banner    string             `bson: "banner" json:"banner, omitempty"`
+	Biography string             `bson:"biography" json:"biography, omitempty"`
+	Location  string             `bson:"location" json:"location, omitempty"`
+	Website   string             `bson:"website" json:"website, omitempty"`
+}
