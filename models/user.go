ac7f76d834315317215add28207a445b7118b8c4
Jean C. Contreras <jean.cntrs@gmail.com>
2020-06-02 21:20:55 -0400

    The user model was modified

--- models/user.go
+++ models/user.go
@@ -8,15 +8,15 @@ import (
 
 // User : Es el modelo de usuario de la base de datos en MongoDB
 type User struct {
-	ID        primitive.ObjectID `bson: "_id, omitempty" json:"id"`
-	Names     string             `bson: "names" bson: "names, omitempty"`
-	Surnames  string             `bson: "surnames" bson: "surnames, omitempty"`
-	Birthdate time.Time          `bson: "birthdate" json:"birthdate", omitempty`
-	Email     string             `bson:"email" json:"email"`
-	Password  string             `bson:"password" json:"password, omitempty"`
-	Avatar    string             `bson: "avatar" json:"avatar, omitempty"`
-	Banner    string             `bson: "banner" json:"banner, omitempty"`
-	Biography string             `bson:"biography" json:"biography, omitempty"`
-	Location  string             `bson:"location" json:"location, omitempty"`
-	Website   string             `bson:"website" json:"website, omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Names     string             `bson:"names" json:"names,omitempty"`
+	Surnames  string             `bson:"surnames" json:"surnames,omitempty"`
+	Birthdate time.Time          `bson:"birthdate" json:birthdate",omitempty`
+	Email     string             `bson:"email" json:email"`
+	Password  string             `bson:"password" json:password,omitempty"`
+	Avatar    string             `bson:"avatar" json:avatar,omitempty"`
+	Banner    string             `bson:"banner" json:banner,omitempty"`
+	Biography string             `bson:"biography" json:biography,omitempty"`
+	Location  string             `bson:"location" json:location,omitempty"`
+	Website   string             `bson:"website" json:website,omitempty"`
 }
